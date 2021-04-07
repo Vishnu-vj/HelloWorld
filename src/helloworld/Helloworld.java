6f8a404affe08b067731e860319f6071816054ac
VHM5COB <VHM5COB@COB-C-002R6.cob.apac.bosch.com>
2021-04-07 10:30:39 +0530

    First Commit...

---
+++ src/helloworld/Helloworld.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) Robert Bosch GmbH. All rights reserved.
+ */
+package helloworld;
+
+
+/**
+ * @author VHM5COB
+ */
+public class Helloworld {
+
+  /**
+   * @param args
+   */
+  public static void main(final String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Hello World!!");
+  }
+
+}
